b7233b7d9123baab0c81b2ec404def738d15d73d
Manish Kumar <95077592+avaremanish@users.noreply.github.com>
2024-03-26 22:52:00 +0530

    update
    schema on image, category, brand ,date

--- models/Products.js
+++ models/Products.js
@@ -3,10 +3,15 @@ const mongoose = require('mongoose')
 
 
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    features: String,
-    description: String,
-}) 
+  name: String,
+  features: String,
+  description: String,
+  image: String,
+  category: String,
+  subcategory: String,
+  brand: String,
+    date: String,
+}); 
 
 const ProductModel = mongoose.model("products", ProductSchema)
 module.exports = ProductModel 
