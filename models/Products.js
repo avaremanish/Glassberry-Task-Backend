aa9ab8e83798f109eba638d831cfbb11551a6a43
Manish Kumar <95077592+avaremanish@users.noreply.github.com>
2024-03-27 02:56:27 +0530

    preview popover and catergories

--- models/Products.js
+++ models/Products.js
@@ -3,14 +3,53 @@ const mongoose = require('mongoose')
 
 
 const ProductSchema = new mongoose.Schema({
-  name: String,
-  features: String,
-  description: String,
-  image: String,
-  category: String,
-  subcategory: String,
-  brand: String,
-    date: String,
+  //   name: String,
+  //   features: String,
+  //   description: String,
+  //   images:[
+  //     {
+  //       data: Buffer, // Image data as Buffer
+  //       contentType: String, // Mime type of the image
+  //     },
+  //   ],
+  //   category: String,
+  //   subcategory: String,
+  //   brand: String,
+  //   date: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  features: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  images: [
+    {
+      data: Buffer, // Image data as Buffer
+      contentType: String, // Mime type of the image
+    },
+  ],
+  category: {
+    type: String,
+    required: true,
+  },
+  subcategory: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    // default: Date.now,
+  },
 }); 
 
 const ProductModel = mongoose.model("products", ProductSchema)
